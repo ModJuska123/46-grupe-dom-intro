58ed0085c75ae50e7b9affa034bfb2b73d375884
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-10-30 18:37:12 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -2,4 +2,15 @@ console.log('Labas rytas Lietuva!')
 const titleDOM = document.getElementById('title');
 console.log(titleDOM);
 
-titleDOM.innerText = Labas rytas Lietuva!
+titleDOM.innerText = 'Hi';
+
+console.log(titleDOM.classList);
+
+for (let i = 0; i < titleDOM.classList.length; i++) {
+    const c = titleDOM.classList[i];
+    titleDOM.classList.remove(c);
+    }
+
+const sectionTitle = "About you my darling";
+const sectionTitleDOM = document.querySelector('h2.section-title > span');
+sectionTitleDOM.textContent = sectionTitle;
